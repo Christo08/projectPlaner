6d5abfa24aa5d6a3f9d66b0abb02f9b69f4212aa
Christo <ChristoOpperman0@gmail.com>
2022-01-16 12:22:49 +0200

    Add ids and show the UI

--- src/com/project/planer/ui/controllers/DetailsViewController.java
+++ src/com/project/planer/ui/controllers/DetailsViewController.java
@@ -1,4 +1,83 @@
 package com.project.planer.ui.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class DetailsViewController {
+
+    @FXML
+    private TabPane detailPanes;
+
+    @FXML
+    private TextArea projectDescriptionTxtA;
+
+    @FXML
+    private TextField projectNameTxtF;
+
+    @FXML
+    private DatePicker projectStartDatumDP;
+
+    @FXML
+    private DatePicker projectStopDatumDP;
+
+    @FXML
+    private Button addSatusBtn;
+
+    @FXML
+    private Button projectAddOrChangeBtn;
+
+    @FXML
+    private Button projectResetBtn;
+
+    @FXML
+    private TextArea clientAddressTxtA;
+
+    @FXML
+    private TextField clientNameTxtF;
+
+    @FXML
+    private Button clientChangeBtn;
+
+    @FXML
+    private Button clientResetBtn;
+
+    @FXML
+    private TextField clientSurnameTxtF;
+
+    @FXML
+    private TextField clientContantNumberTxtF;
+
+    @FXML
+    private TextField companyNameTxtF;
+
+    @FXML
+    void addAStatusToProject(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addOrChangeProject(ActionEvent event) {
+
+    }
+
+    @FXML
+    void changeClientDetails(ActionEvent event) {
+
+    }
+
+    @FXML
+    void resetClientDetails(ActionEvent event) {
+
+    }
+
+    @FXML
+    void resetProjectDetail(ActionEvent event) {
+
+    }
+
 }
